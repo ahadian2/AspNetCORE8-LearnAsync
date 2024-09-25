517f281c0641bf585b73e0929923a3c1607c8fcf
Mohammadreza <Mohammadreza@DESKTOP-PCF2MPJ>
2024-09-25 10:02:59 +0330

    Add project files.

---
+++ LearnAsync/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LearnAsync.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            int x = Sum().Result;
+            return View();
+        }
+
+
+        public async Task<IActionResult> GetUserAsync(CancellationToken cancellationToken)
+        {
+            await Task.Delay(5000, cancellationToken);
+            return View("GetUserAsync");
+        }
+
+        public async Task<IActionResult> ListUserAsync()
+        {
+            int sum = 0;
+            await Task.Run(() => {
+
+                for (int i = 0; i < 10; i++) { 
+                
+                sum+= i;
+                }
+            });
+            return View();
+        }
+
+        private async Task<int> Sum()
+        {
+            int sum = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                sum += i;
+                await Task.Delay(100);
+            }
+            return sum;
+        }
+
+    }
+}
---
+++ LearnAsync/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LearnAsync.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
